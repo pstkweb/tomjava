dd3f31533b3b45fc5b67a4fd24487bd2db4fe798
crazyspyd3r@gmail.com <crazyspyd3r@gmail.com@c6a44206-86dc-946e-20fb-f1433a0f9dd5>
2010-12-02 08:56:55 +0000

    Début gestion des collisions <Mais pas l'autre côté hein THOMAS!!!!!!!!!>

--- Phrastique/src/controleur/ControleTextuelle.java
+++ Phrastique/src/controleur/ControleTextuelle.java
@@ -14,6 +14,7 @@ public class ControleTextuelle implements MouseListener, MouseMotionListener {
 	private LinkedList<Component> relations;
 	private Point positionDepart;
 	private Point positionClick;
+	private Point positionAnterieure = null;
 	
 	public ControleTextuelle(VueTextuelle texte){
 		this.texte = texte;
@@ -61,20 +62,70 @@ public class ControleTextuelle implements MouseListener, MouseMotionListener {
 					(relX > compX+compWidth && relY > compY && relY < compY+compHeight) ||
 					(relX > compX+compWidth && relY > compY && relY+relHeight < compY+compHeight)){
 				possible = false;	
-		}
+			}
 			i++;
 		}while(i<texte.getComponents().length && possible);
 		if(!possible){
 			e.getComponent().setLocation(positionDepart);
 			texte.repaint();
 		}
-		
+		positionAnterieure = null;
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e){
-		e.getComponent().setLocation(e.getComponent().getX()+e.getX()-positionClick.x,
-										e.getComponent().getY()+e.getY()-positionClick.y);
+		/**
+		 * si composant au meme endroit qu'un autre composant
+		 * alors replacer à l'endroit précédent
+		 */
+		boolean possible = true;
+		Point positionRel = new Point(e.getComponent().getX()+e.getX()-positionClick.x, 
+				e.getComponent().getY()+e.getY()-positionClick.y);
+		Point positionComp;
+		int i = 0;
+		int relX = positionRel.x;
+		int relY = positionRel.y;
+		int relWidth = e.getComponent().getWidth();
+		int relHeight = e.getComponent().getHeight();
+		
+		do{
+			positionComp = new Point(texte.getComponents()[i].getX(), texte.getComponents()[i].getY());
+			int compX = positionComp.x;
+			int compY = positionComp.y;
+			int compWidth = texte.getComponents()[i].getWidth();
+			int compHeight = texte.getComponents()[i].getHeight();
+			
+			/**
+			 * combinaisons possible pour le que le placement ne soit pas autorisé
+			 */
+			if((relX+relWidth > compX && relY+relHeight > compY && relY+relHeight < compY+compHeight) ||
+					(relX+relWidth > compX && relY > compY && relY < compY+compHeight) ||
+					(relX+relWidth > compX && relY > compY && relY+relHeight < compY+compHeight) ||
+					(relX > compX+compWidth && relY+relHeight > compY && relY+relHeight < compY+compHeight) ||
+					(relX > compX+compWidth && relY > compY && relY < compY+compHeight) ||
+					(relX > compX+compWidth && relY > compY && relY+relHeight < compY+compHeight)){
+				possible = false;
+			}
+			if(e.getComponent().equals(texte.getComponents()[i])){
+				possible = true;
+			}
+			i++;
+		}while(i<texte.getComponents().length && possible);
+		if(!possible){
+			if(positionAnterieure == null){
+				System.out.println("impossible Départ");
+				e.getComponent().setLocation(positionDepart);
+			}
+			else{
+				System.out.println("impossible Antérieure");
+				e.getComponent().setLocation(positionAnterieure);
+			}
+		}
+		else{
+			System.out.println("possible");
+			positionAnterieure = positionRel;
+			e.getComponent().setLocation(positionRel);
+		}
 		texte.repaint();
 	}
 
